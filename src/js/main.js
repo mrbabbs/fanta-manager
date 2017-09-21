e69130cf586197cf40cd5e9387a53ef3b187fa50
Giovanni Esposito <g.esposito@instamotion.com>
2017-09-21 23:07:46 +0200

    Add selects dinamically

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
 (function () {
   const fp = Utils.fp;
   const TEAM_CONFIG = {
-    "P": { count: 3, id: "goalkeepers" },
-    "D": { count: 8, id: "defensers" },
-    "M": { count: 8, id: "middlefielders" },
-    "S": { count: 6, id: "strikers"},
+    "P": { count: 3, position: "goalkeepers", players: [] },
+    "D": { count: 8, position: "defensers", players: [] },
+    "M": { count: 8, position: "middlefielders", players: [] },
+    "S": { count: 6, position: "strikers", players: [] },
   };
   const MY_TEAM = 'myTeam';
   const FORM_ID = "#myTeam";
@@ -17,6 +17,37 @@
     mark: 0,
     markF: 0,
   };
+
+  const optionPlayer =
+    name => `<option value="${name}">${_.capitalize(name)}</option>`;
+
+  function selectPlayer(config) {
+    return `
+    <div class="col-xs-3">
+      <div class="input-group">
+        <select id="${config.position}Select"
+          name="${config.position}Select"
+          class="selectpicker form-control"
+          multiple
+          data-live-search="true"
+          title="Choose a ${_.capitalize(config.position)}"
+          data-selected-text-format="count"
+        >
+        ${config.players.reduce(
+          (acc, curr) => `${acc}${optionPlayer(curr)}`, ''
+        )}
+        </select>
+        <span class="input-group-btn">
+          <button class="btn btn-default" type="button">
+            &nbsp;
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
+            &nbsp;
+          </button>
+        </span>
+      </div>
+    </div>
+    `;
+  }
 
   function playerField(player, editable) {
     return `
@@ -86,9 +117,14 @@
   );
 
   function init() {
-    const myTeam = Utils.load(MY_TEAM);
-    fp.map(appendPlayer, myTeam || generateEmptyTeam(TEAM_CONFIG));
-    const form = Utils.getContainer(FORM_ID);
+    // const myTeam = Utils.load(MY_TEAM);
+    // fp.map(appendPlayer, myTeam || generateEmptyTeam(TEAM_CONFIG));
+    // const form = Utils.getContainer(FORM_ID);
+    const containerSelectors = Utils.getContainer('.container-selectors');
+    _.mapValues(
+      TEAM_CONFIG,
+      fp.compose(appendNode(containerSelectors), selectPlayer, Utils.log('hello'))
+    );
 
     form.addEventListener('submit', ev => {
       ev.preventDefault();
